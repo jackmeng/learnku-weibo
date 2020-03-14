c3b76b431a2098a9ccecdb0d0e94956dfaa505ec
jackmeng <jiekemeng@gmail.com>
2020-03-14 11:59:17 +0800

    中文的错误提示信息

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -158,7 +158,8 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+//        Illuminate\Translation\TranslationServiceProvider::class, // 英文包
+        Overtrue\LaravelLang\TranslationServiceProvider::class, // 中文包
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
