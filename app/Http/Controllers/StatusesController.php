fa9a18e8e5c03c793b0e330ecad9828a59b05228
vagrant <vagrant@homestead>
2020-03-14 22:55:57 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -23,6 +24,14 @@ class StatusesController extends Controller
         ]);
         session()->flash('success', '发布成功！');
 
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
